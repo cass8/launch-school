33e91eab674a830c756eda3118afabe969ee106b
cass8 <casseh91@gmail.com>
2021-05-09 15:21:51 +0100

    updating mortgage calculator

--- lesson_2/mortgage_calculator.rb
+++ lesson_2/mortgage_calculator.rb
@@ -1,3 +1,6 @@
+require 'yaml'
+MESSAGES = YAML.load_file('mortgage_calculator_messages.yml')
+
 # take loan amount
 # take the APR (annual percentage rate)
 # take loan duration
@@ -22,7 +25,7 @@ def valid_integer?(num)
 end
 
 def valid_months?(num)
-  num.to_i() < 12
+  num.to_i() < 12 && num.to_i() >= 0
 end
 
 def valid_loan?(num)
@@ -33,73 +36,80 @@ def valid_float?(num)
   num.to_f().to_s() == num
 end
 
-prompt("Hello and welcome to the mortgage calculator!")
+prompt(MESSAGES['welcome'])
 
 name = ''
+prompt(MESSAGES['name'])
 loop do
-  prompt("Please enter your name:")
   name = Kernel.gets().chomp()
   if name.empty?()
-    puts "You must enter your name!"
+    prompt(MESSAGES['name_insist'])
   else
     break
   end
 end
 
 prompt("Hi #{name}!")
-prompt("I just need a few more details...")
 
-loan_amount = ''
 loop do
-  prompt("How much are you borrowing?(£)")
-  prompt("Please enter a number between 1-999999")
-  loan_amount = Kernel.gets().chomp().to_i()
-  if valid_integer?(loan_amount) || valid_loan?(loan_amount)
-    break
-  else
-    prompt("You must enter a valid whole number between 1-999999")
+  prompt(MESSAGES['details'])
+  prompt(MESSAGES['borrowing'])
+  prompt(MESSAGES['loan_amount'])
+  loan_amount = ''
+  loop do
+    loan_amount = Kernel.gets().chomp().to_i()
+    if valid_integer?(loan_amount) && valid_loan?(loan_amount)
+      break
+    else
+      prompt(MESSAGES['loan_amount_insist'])
+    end
   end
-end
 
-apr = ''
-loop do
-  prompt("Please enter the expected APR (Annual Percentage Rate)")
-  apr = Kernel.gets().chomp()
-  if valid_float?(apr) || valid_integer?(apr)
-    break
-  else
-    prompt("You must enter a valid value!")
+  apr = ''
+  prompt(MESSAGES['apr'])
+  loop do
+    apr = Kernel.gets().chomp()
+    if valid_float?(apr) || valid_integer?(apr)
+      break
+    else
+      prompt(MESSAGES['apr_insist'])
+    end
   end
-end
 
-loan_duration_years = ''
-loop do
-  prompt("Please enter the loan duration (years)")
-  loan_duration_years = Kernel.gets().chomp()
-  if valid_integer?(loan_duration_years)
-    break
-  else
-    prompt("You must enter a valid number of years")
+  loan_duration_years = ''
+  prompt(MESSAGES['loan_duration_years'])
+  loop do
+    loan_duration_years = Kernel.gets().chomp()
+    if valid_integer?(loan_duration_years)
+      break
+    else
+      prompt(MESSAGES['loan_duration_years_insist'])
+    end
   end
-end
 
-loan_duration_months = ''
-loop do
-  prompt("And how many months?")
-  loan_duration_months = Kernel.gets().chomp()
-  if valid_integer?(loan_duration_months) || valid_months?(loan_duration_months)
-    break
-  else
-    prompt("You must enter a valid number of months!")
+  loan_duration_months = ''
+  loop do
+    prompt(MESSAGES['loan_duration_months'])
+    loan_duration_months = Kernel.gets().chomp()
+    if valid_months?(loan_duration_months)
+      break
+    else
+      prompt(MESSAGES['loan_duration_months_insist'])
+    end
   end
+
+  monthly_interest_rate = (apr.to_f() / 100) / 12
+
+  loan_duration_total = (loan_duration_years.to_i() * 12) +
+                        loan_duration_months.to_i()
+
+  monthly_payment = loan_amount * (monthly_interest_rate /
+                    (1 - (1 + monthly_interest_rate)**(-loan_duration_total)))
+
+  prompt("Your monthly payment will be £#{monthly_payment.round(2)}.")
+
+  prompt(MESSAGES['again'])
+  another_calculation = Kernel.gets().chomp()
+  break unless another_calculation.downcase.start_with?('y')
 end
-
-monthly_interest_rate = (apr.to_f() / 100) / 12
-
-loan_duration_total = (loan_duration_years.to_i() * 12) +
-                      loan_duration_months.to_i()
-
-monthly_payment = loan_amount * (monthly_interest_rate /
-                  (1 - (1 + monthly_interest_rate)**(-loan_duration_total)))
-
-prompt("Your monthly payment will be £#{monthly_payment.round(2)}.")
+prompt(MESSAGES['goodbye'])
